652aa742e9e17dee75418278aa19806c0b8afcf8
Alexander Hersh <alexander.hersh@metaswitch.com>
2015-09-14 17:54:36 -0700

    main program no longer daemonized, multiple watcher threads, first ns policy

--- calico_kubernetes/calico_daemon.py
+++ calico_kubernetes/calico_daemon.py
@@ -1,55 +1,230 @@
 #!/usr/bin/python
 import os
+import sys
 import time
 import json
-import requests
 import Queue
+import logging
+import requests
+import pycalico
+
 from threading import Thread
 from subprocess import check_output
 from contextlib import closing
-from daemon import runner
+from pycalico.datastore import DatastoreClient, PROFILE_PATH
+from pycalico.datastore_datatypes import Rules
 
 KUBE_API_ROOT = os.environ.get('KUBE_API_ROOT',
-                               'http://kubernetes-master:8080/api/v1/')
-POLICY_LOG_DIR = "/var/log/calico/calico-policy"
-POLICY_LOG = "%s/calico-policy.log"
+                               'http://localhost:8080/api/v1/')
 
-class PolicyDaemon():
+POLICY_LOG_DIR = "/var/log/calico/calico-policy"
+POLICY_LOG = "%s/calico-policy.log" % POLICY_LOG_DIR
+
+LOG_FORMAT = '%(asctime)s %(process)d %(levelname)s %(filename)s: %(message)s'
+LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO').upper()
+
+_log = logging.getLogger(__name__)
+
+
+class PolicyAgent():
 
     def __init__(self):
-        self.stdin_path = '/dev/null'
-        self.stdout_path = '/dev/tty'
-        self.stderr_path = '/dev/tty'
-        self.pidfile_path = '/tmp/calico-policy.pid'
-        self.pidfile_timeout = None
+        self.client = DatastoreClient()
         self.q = Queue.Queue()
-        self.watcher = Thread(target=_keep_watch, args=(self.q, "pods"))
-        self.watcher.daemon = True
+        self.pods = dict()
+        self.services = dict()
+        self.endpoints = dict()
+        self.namespaces = dict()
+
+        self.PodWatcher = Thread(target=_keep_watch,
+                                 args=(self.q, "pods"))
+        self.SvcWatcher = Thread(target=_keep_watch,
+                                 args=(self.q, "services"))
+        self.NsWatcher = Thread(target=_keep_watch,
+                                args=(self.q, "namespaces"))
+        self.EptsWatcher = Thread(target=_keep_watch,
+                                  args=(self.q, "endpoints"))
+
+        self.PodWatcher.daemon = True
+        self.SvcWatcher.daemon = True
+        self.NsWatcher.daemon = True
+        self.EptsWatcher.daemon = True
 
     def run(self):
-        self.watcher.start()
+        self.PodWatcher.start()
+        self.SvcWatcher.start()
+        self.NsWatcher.start()
+        self.EptsWatcher.start()
 
         while True:
             self.read_responses()
-            time.sleep(1)
 
     def read_responses(self):
         try:
             response = self.q.get_nowait()
-            print(time.asctime(time.localtime()))
-            print(response)
+            r_json = json.loads(response)
+            r_type = r_json["type"]
+            r_obj = r_json["object"]
+            r_kind = r_obj["kind"]
+
+            _log.info("%s: %s" % (r_type, r_kind))
+
+            if r_kind in ["Pod", "Namespace", "Service"]:
+                self.process_resource(action=r_type, kind=r_kind, target=r_obj)
+            else:
+                _log.info(
+                    "Resource %s not Pod, Service, or Namespace" % r_kind)
 
         except Queue.Empty:
             pass
 
+    def process_resource(self, action, kind, target):
+        """
+        Takes a target object and an action and updates internal list
+        """
+        # Determine Resource Kind
+        if kind == "Pod":
+            resource_bin = self.pods
+            obj = Pod(target)
+
+        elif kind == "Service":
+            resource_bin = self.services
+            obj = Service(target)
+
+        elif kind == "Endpoints":
+            resource_bin = self.endpoints
+            obj = Endopints(target)
+
+        elif kind == "Namespace":
+            resource_bin = self.namespaces
+            obj = Namespace(target)
+
+        else:
+            _log.error("resource %s not Pod, Service, or Namespace" % kind)
+            sys.exit(1)
+
+        target_key = obj.key
+
+        if action == "ADDED":
+            if target_key not in resource_bin:
+                resource_bin[target_key] = obj
+                _log.info("%s added to Calico store" % target_key)
+
+            else:
+                _log.error("Tried to Add %s, but %s already in bin" %
+                           (obj, target_key))
+
+        elif action == "DELETED":
+            if target_key in resource_bin:
+                del resource_bin[target_key]
+                _log.info("%s deleted from Calico store" % target_key)
+
+            else:
+                _log.error(
+                    "Tried to Delete %s, but %s was not in bin" %
+                    (obj, target_key))
+
+        elif action == "MODIFIED":
+            if target_key in resource_bin:
+                _log.info("Updating %s\n%s\n=====>\n%s" %
+                          (target_key, resource_bin[target_key], obj))
+                resource_bin[target_key] = obj
+
+            else:
+                _log.warning("Tried to Modify %s, but %s was not in bin. "
+                             "Treating as Addition" %
+                             (target_key, target_key))
+                self.process_resource(action="ADDED", kind=kind, target=obj)
+
+    def define_namespace_policy(self, namespace):
+        ns_tag = "namespace_%s" % namespace.name
+        profile_path = PROFILE_PATH % {"profile_id": ns_tag}
+        self.client.etcd_client.write(profile_path + "tags", '["%s"]' % ns_tag)
+        default_allow = Rule(action="allow")
+
+        if namespace.policy == "open":
+            rules = Rules(id=ns_tag,
+                      inbound_rules=[default_allow],
+                      outbound_rules=[default_allow])
+
+        elif namespace.policy == "closed":
+            rules = Rules(id=ns_tag,
+                      inbound_rules=[Rule(action="allow", src_tag=ns_tag)],
+                      outbound_rules=[default_allow])
+
+        else:
+            _log.error("Namespace %s policy is neither open nor closed" % namespace.name)
+            sys.exit(1)
+        
+        self.client.etcd_client.write(profile_path + "rules", rules.to_json())
+
+
+class Resource():
+    def __init__(self, json):
+        self.json = json
+        self.from_json(json)
+        self.key = self.get_key()
+        self.needs_resync = True
+
+    def from_json(self, json):
+        self.uid = None
+        return
+
+    def get_key(self):
+        return self.uid
+
+    def resync(self):
+        self.needs_resync = False
+
+
+class Pod(Resource):
+    def from_json(self, json):
+        self.kind = "Pod"
+        self.uid = json["metadata"]["uid"]
+        self.name = json["metadata"]["name"]
+        self.namespace = json["metadata"]["namespace"]
+
+
+class Service(Resource):
+    def from_json(self, json):
+        self.kind = "Service"
+        self.uid = json["metadata"]["uid"]
+        self.name = json["metadata"]["name"]
+        self.namespace = json["metadata"]["namespace"]
+
+
+class Endpoints(Resource):
+    def from_json(self, json):
+        self.kind = "Endopints"
+        self.selfLink = json["metadata"]["selfLink"]
+        self.items = json["items"]
+
+    def get_key(self):
+        return self.selfLink
+
+
+class Namespace(Resource):
+    def from_json(self, json):
+            self.kind = "Namespace"
+        try:
+            self.uid = json["metadata"]["uid"]
+            self.name = json["metadata"]["name"]
+        except KeyError:
+            _log.error("Namespace does not have uid or name")
+
+        try:
+            self.policy = json["metadata"]["annotations"]["projectcalico.org/policy"]
+        except KeyError:
+            _log.warning("Namespace does not have policy, assumed open")
+            self.policy = "open"
+
 
 def _keep_watch(queue, path):
-    # print("keep watch")
-    response = _get_api_path("%s?watch=true" % path)
-    # print("response %s" % response.text)
+    response = _get_api_path("watch/%s" % path)
     for line in response.iter_lines():
         if line:
             queue.put(line)
+
 
 def _get_api_token():
     """
@@ -67,6 +242,7 @@ def _get_api_token():
     auth_data = json.loads(json_string)
     return auth_data['BearerToken']
 
+
 def _get_api_path(path):
     """Get a resource from the API specified API path.
 
@@ -81,8 +257,18 @@ def _get_api_path(path):
     session = requests.Session()
     session.headers.update({'Authorization': 'Bearer ' + bearer_token})
     session = requests.Session()
-    return session.get("%s%s" % (KUBE_API_ROOT, path), verify=False, stream=True)
+    return session.get("%s%s" % (KUBE_API_ROOT, path),
+                       verify=False, stream=True)
 
-app = PolicyDaemon()
-daemon_runner = runner.DaemonRunner(app)
-daemon_runner.do_action()
+if __name__ == '__main__':
+
+    if not os.path.exists(POLICY_LOG_DIR):
+        os.makedirs(POLICY_LOG_DIR)
+
+    hdlr = logging.FileHandler(filename=POLICY_LOG)
+    hdlr.setFormatter(logging.Formatter(LOG_FORMAT))
+    _log.addHandler(hdlr)
+    _log.setLevel(LOG_LEVEL)
+
+    app = PolicyAgent()
+    app.run()
